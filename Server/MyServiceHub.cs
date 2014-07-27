48e0b7547b18e796e54d9e905d857463b859d2e1
RenYueHD <renyuehd@gmail.com>
2014-07-27 12:11:01 +0800

    实现跨页面Session,添加Web客户端

--- Server/MyServiceHub.cs
+++ Server/MyServiceHub.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.SignalR;
 using SignalRExtendLib;
 using SignalRExtendLib.Filter;
+using SignalRExtendLib.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +11,10 @@ using System.Threading.Tasks;
 namespace Server
 {
     [SessionEnable]
-    public class MyServiceHub : Hub
+    public class MyServiceHub : Hub, ICookieDistrinctRequest
     {
+
+
         public void HelloWorld()
         {
             Console.WriteLine("ID为" + base.Context.ConnectionId + "的用户进入聊天室");
@@ -25,13 +28,25 @@ namespace Server
 
         public void SendObject(dynamic obj)
         {
-            Console.WriteLine("客户端发送了一个对象,类型为"+obj.GetType().Name.ToString());
-            Clients.Caller.pushMessage("管理员", "您向我发送了一个类型为"+obj.GetType().Name.ToString()+"的对象");
+            Console.WriteLine("客户端发送了一个对象,类型为" + obj.GetType().Name.ToString());
+            Clients.Caller.pushMessage("管理员", "您向我发送了一个类型为" + obj.GetType().Name.ToString() + "的对象");
         }
 
         public void GetSessionId()
         {
-            Clients.Caller.pushMessage("SessionID",Context.RequestCookies["ASP.NET_Signal_SessionKey"].Value);
+            Clients.Caller.pushMessage("SessionID", Context.RequestCookies["ASP.NET_SignalRExtendLib_SessionKey"].Value);
+        }
+
+        public void AppendCookie(string ckName, string ckValue, DateTime? expire = null)
+        {
+            if (expire.HasValue)
+            {
+                Clients.Caller.addCookie(ckName, ckValue, (expire.Value - new DateTime(1970, 1, 1)).TotalMilliseconds);
+            }
+            else
+            {
+                Clients.Caller.addCookie(ckName, ckValue);
+            }
         }
     }
 }
